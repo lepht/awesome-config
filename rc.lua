5e83d7b592a8415fc45db7b7ba9e03ba88ef183a
lepht <pete.clark@gmail.com>
2011-05-03 01:43:22 -0400

    Removes default clock

--- rc.lua
+++ rc.lua
@@ -164,8 +164,6 @@ end
 -- }}}
 
 -- {{{ Wibox
--- Create a textclock widget
-mytextclock = awful.widget.textclock({ align = "right" })
 
 -- Create a systray
 mysystray = widget({ type = "systray" })
