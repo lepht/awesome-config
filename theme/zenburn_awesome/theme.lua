42f4c2eca5ad7a2a80bd66af386024fde087d70c
lepht <pete.clark@gmail.com>
2011-05-03 01:50:27 -0400

    Changes image references to theme files, instead of default icons in /usr/share

--- theme/zenburn_awesome/theme.lua
+++ theme/zenburn_awesome/theme.lua
@@ -111,55 +111,55 @@ theme.menu_width  = "100"
 
 -- {{{ Icons
 -- {{{ Taglist
-theme.taglist_squares_sel   = "/usr/share/awesome/themes/zenburn/taglist/squarefz.png"
-theme.taglist_squares_unsel = "/usr/share/awesome/themes/zenburn/taglist/squarez.png"
+theme.taglist_squares_sel   = "/home/lepht/github/awesome-config/theme/zenburn_awesome/taglist/squarefz.png"
+theme.taglist_squares_unsel = "/home/lepht/github/awesome-config/theme/zenburn_awesome/taglist/squarez.png"
 --theme.taglist_squares_resize = "false"
 -- }}}
 
 -- {{{ Misc
-theme.awesome_icon           = "/usr/share/awesome/themes/zenburn/awesome-icon.png"
+theme.awesome_icon           = "/home/lepht/github/awesome-config/theme/zenburn_awesome/awesome-icon.png"
 theme.menu_submenu_icon      = "/usr/share/awesome/themes/default/submenu.png"
 theme.tasklist_floating_icon = "/usr/share/awesome/themes/default/tasklist/floatingw.png"
 -- }}}
 
 -- {{{ Layout
-theme.layout_tile       = "/usr/share/awesome/themes/zenburn/layouts/tile.png"
-theme.layout_tileleft   = "/usr/share/awesome/themes/zenburn/layouts/tileleft.png"
-theme.layout_tilebottom = "/usr/share/awesome/themes/zenburn/layouts/tilebottom.png"
-theme.layout_tiletop    = "/usr/share/awesome/themes/zenburn/layouts/tiletop.png"
-theme.layout_fairv      = "/usr/share/awesome/themes/zenburn/layouts/fairv.png"
-theme.layout_fairh      = "/usr/share/awesome/themes/zenburn/layouts/fairh.png"
-theme.layout_spiral     = "/usr/share/awesome/themes/zenburn/layouts/spiral.png"
-theme.layout_dwindle    = "/usr/share/awesome/themes/zenburn/layouts/dwindle.png"
-theme.layout_max        = "/usr/share/awesome/themes/zenburn/layouts/max.png"
-theme.layout_fullscreen = "/usr/share/awesome/themes/zenburn/layouts/fullscreen.png"
-theme.layout_magnifier  = "/usr/share/awesome/themes/zenburn/layouts/magnifier.png"
-theme.layout_floating   = "/usr/share/awesome/themes/zenburn/layouts/floating.png"
+theme.layout_tile       = "/home/lepht/github/awesome-config/theme/zenburn_awesome/layouts/tile.png"
+theme.layout_tileleft   = "/home/lepht/github/awesome-config/theme/zenburn_awesome/layouts/tileleft.png"
+theme.layout_tilebottom = "/home/lepht/github/awesome-config/theme/zenburn_awesome/layouts/tilebottom.png"
+theme.layout_tiletop    = "/home/lepht/github/awesome-config/theme/zenburn_awesome/layouts/tiletop.png"
+theme.layout_fairv      = "/home/lepht/github/awesome-config/theme/zenburn_awesome/layouts/fairv.png"
+theme.layout_fairh      = "/home/lepht/github/awesome-config/theme/zenburn_awesome/layouts/fairh.png"
+theme.layout_spiral     = "/home/lepht/github/awesome-config/theme/zenburn_awesome/layouts/spiral.png"
+theme.layout_dwindle    = "/home/lepht/github/awesome-config/theme/zenburn_awesome/layouts/dwindle.png"
+theme.layout_max        = "/home/lepht/github/awesome-config/theme/zenburn_awesome/layouts/max.png"
+theme.layout_fullscreen = "/home/lepht/github/awesome-config/theme/zenburn_awesome/layouts/fullscreen.png"
+theme.layout_magnifier  = "/home/lepht/github/awesome-config/theme/zenburn_awesome/layouts/magnifier.png"
+theme.layout_floating   = "/home/lepht/github/awesome-config/theme/zenburn_awesome/layouts/floating.png"
 -- }}}
 
 -- {{{ Titlebar
-theme.titlebar_close_button_focus  = "/usr/share/awesome/themes/zenburn/titlebar/close_focus.png"
-theme.titlebar_close_button_normal = "/usr/share/awesome/themes/zenburn/titlebar/close_normal.png"
+theme.titlebar_close_button_focus  = "/home/lepht/github/awesome-config/theme/zenburn_awesome/titlebar/close_focus.png"
+theme.titlebar_close_button_normal = "/home/lepht/github/awesome-config/theme/zenburn_awesome/titlebar/close_normal.png"
 
-theme.titlebar_ontop_button_focus_active  = "/usr/share/awesome/themes/zenburn/titlebar/ontop_focus_active.png"
-theme.titlebar_ontop_button_normal_active = "/usr/share/awesome/themes/zenburn/titlebar/ontop_normal_active.png"
-theme.titlebar_ontop_button_focus_inactive  = "/usr/share/awesome/themes/zenburn/titlebar/ontop_focus_inactive.png"
-theme.titlebar_ontop_button_normal_inactive = "/usr/share/awesome/themes/zenburn/titlebar/ontop_normal_inactive.png"
+theme.titlebar_ontop_button_focus_active  = "/home/lepht/github/awesome-config/theme/zenburn_awesome/titlebar/ontop_focus_active.png"
+theme.titlebar_ontop_button_normal_active = "/home/lepht/github/awesome-config/theme/zenburn_awesome/titlebar/ontop_normal_active.png"
+theme.titlebar_ontop_button_focus_inactive  = "/home/lepht/github/awesome-config/theme/zenburn_awesome/titlebar/ontop_focus_inactive.png"
+theme.titlebar_ontop_button_normal_inactive = "/home/lepht/github/awesome-config/theme/zenburn_awesome/titlebar/ontop_normal_inactive.png"
 
-theme.titlebar_sticky_button_focus_active  = "/usr/share/awesome/themes/zenburn/titlebar/sticky_focus_active.png"
-theme.titlebar_sticky_button_normal_active = "/usr/share/awesome/themes/zenburn/titlebar/sticky_normal_active.png"
-theme.titlebar_sticky_button_focus_inactive  = "/usr/share/awesome/themes/zenburn/titlebar/sticky_focus_inactive.png"
-theme.titlebar_sticky_button_normal_inactive = "/usr/share/awesome/themes/zenburn/titlebar/sticky_normal_inactive.png"
+theme.titlebar_sticky_button_focus_active  = "/home/lepht/github/awesome-config/theme/zenburn_awesome/titlebar/sticky_focus_active.png"
+theme.titlebar_sticky_button_normal_active = "/home/lepht/github/awesome-config/theme/zenburn_awesome/titlebar/sticky_normal_active.png"
+theme.titlebar_sticky_button_focus_inactive  = "/home/lepht/github/awesome-config/theme/zenburn_awesome/titlebar/sticky_focus_inactive.png"
+theme.titlebar_sticky_button_normal_inactive = "/home/lepht/github/awesome-config/theme/zenburn_awesome/titlebar/sticky_normal_inactive.png"
 
-theme.titlebar_floating_button_focus_active  = "/usr/share/awesome/themes/zenburn/titlebar/floating_focus_active.png"
-theme.titlebar_floating_button_normal_active = "/usr/share/awesome/themes/zenburn/titlebar/floating_normal_active.png"
-theme.titlebar_floating_button_focus_inactive  = "/usr/share/awesome/themes/zenburn/titlebar/floating_focus_inactive.png"
-theme.titlebar_floating_button_normal_inactive = "/usr/share/awesome/themes/zenburn/titlebar/floating_normal_inactive.png"
+theme.titlebar_floating_button_focus_active  = "/home/lepht/github/awesome-config/theme/zenburn_awesome/titlebar/floating_focus_active.png"
+theme.titlebar_floating_button_normal_active = "/home/lepht/github/awesome-config/theme/zenburn_awesome/titlebar/floating_normal_active.png"
+theme.titlebar_floating_button_focus_inactive  = "/home/lepht/github/awesome-config/theme/zenburn_awesome/titlebar/floating_focus_inactive.png"
+theme.titlebar_floating_button_normal_inactive = "/home/lepht/github/awesome-config/theme/zenburn_awesome/titlebar/floating_normal_inactive.png"
 
-theme.titlebar_maximized_button_focus_active  = "/usr/share/awesome/themes/zenburn/titlebar/maximized_focus_active.png"
-theme.titlebar_maximized_button_normal_active = "/usr/share/awesome/themes/zenburn/titlebar/maximized_normal_active.png"
-theme.titlebar_maximized_button_focus_inactive  = "/usr/share/awesome/themes/zenburn/titlebar/maximized_focus_inactive.png"
-theme.titlebar_maximized_button_normal_inactive = "/usr/share/awesome/themes/zenburn/titlebar/maximized_normal_inactive.png"
+theme.titlebar_maximized_button_focus_active  = "/home/lepht/github/awesome-config/theme/zenburn_awesome/titlebar/maximized_focus_active.png"
+theme.titlebar_maximized_button_normal_active = "/home/lepht/github/awesome-config/theme/zenburn_awesome/titlebar/maximized_normal_active.png"
+theme.titlebar_maximized_button_focus_inactive  = "/home/lepht/github/awesome-config/theme/zenburn_awesome/titlebar/maximized_focus_inactive.png"
+theme.titlebar_maximized_button_normal_inactive = "/home/lepht/github/awesome-config/theme/zenburn_awesome/titlebar/maximized_normal_inactive.png"
 -- }}}
 -- }}}
 
